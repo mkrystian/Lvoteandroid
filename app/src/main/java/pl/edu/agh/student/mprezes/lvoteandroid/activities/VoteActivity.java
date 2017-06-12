47943ec641469d3705f6b6c252867a8ea2b6061d
Krystian Majewski <majewski.kristian@gmail.com>
2017-06-13 00:37:46 +0200

    VoteActivity created

---
+++ app/src/main/java/pl/edu/agh/student/mprezes/lvoteandroid/activities/VoteActivity.java
@@ -0,0 +1,38 @@
+package pl.edu.agh.student.mprezes.lvoteandroid.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import pl.edu.agh.student.mprezes.lvoteandroid.R;
+
+public class VoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vote);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Bundle bundle = getIntent().getExtras();
+
+        TextView votingTitle = (TextView) findViewById(R.id.voting_title);
+        votingTitle.setText(bundle.getString("voting_title"));
+
+    }
+
+}
--- app/src/main/java/pl/edu/agh/student/mprezes/lvoteandroid/activities/listview/AvailableVotingsListAdapter.java
+++ app/src/main/java/pl/edu/agh/student/mprezes/lvoteandroid/activities/listview/AvailableVotingsListAdapter.java
@@ -1,17 +1,21 @@
 package pl.edu.agh.student.mprezes.lvoteandroid.activities.listview;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
 
 import pl.edu.agh.student.mprezes.lvoteandroid.R;
+import pl.edu.agh.student.mprezes.lvoteandroid.activities.VoteActivity;
 import pl.edu.agh.student.mprezes.lvoteandroid.model.voting.Voting;
 
 /**
@@ -49,8 +53,24 @@ public class AvailableVotingsListAdapter extends ArrayAdapter<Voting> {
         }
 
         TextView groupName = (TextView) convertView.findViewById(R.id.voting_name);
+        Button voteButton = (Button) convertView.findViewById(R.id.vote_button);
+        voteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startVoteWindow();
+            }
+        });
         groupName.setText(voting != null ? voting.getName() : null);
 
         return result;
+    }
+
+    private void startVoteWindow() {
+        Intent intent = new Intent(context, VoteActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("voting_title", "Some title");
+        intent.putExtras(bundle);
+
+        context.startActivity(intent);
     }
 }
